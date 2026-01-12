3bb46b1d6912ae6da3e1aee2cf4c245788b0258d
Meng <nslogmeng@gmail.com>
2026-01-12 14:08:58 +0800

    chore: add Swift-DocC plugin dependency and configure plugin for Service target
    - Added a dependency on the swift-docc-plugin to enable documentation generation.
    - Configured the Service target to use the Swift-DocC plugin for improved documentation capabilities.

--- Package.swift
+++ Package.swift
@@ -17,6 +17,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "Service", targets: ["Service"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", .upToNextMajor(from: "1.4.5")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -24,6 +27,9 @@ let package = Package(
             name: "Service",
             swiftSettings: [
                 .enableUpcomingFeature("MemberImportVisibility")
+            ],
+            plugins: [
+                .plugin(name: "Swift-DocC", package: "swift-docc-plugin")
             ]
         ),
         .testTarget(name: "ServiceTests", dependencies: ["Service"]),
